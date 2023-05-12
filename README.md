56006b889f3f9cb082d908a67ee74955e63dadc5
ChezyName <mineboy2468@gmail.com>
2023-05-11 23:29:19 -0500

    Finished in 1hr

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Discord-x-Roblox-Snitching
- A Discord Bot That Tells You When A User Is On Roblox But Not On Discord
+A Discord Bot That Tells You When A User Is On Roblox!
+
+Imagine you have a friend who likes to be on Roblox but ever joins VC (Voice Chat),
+You can now use this Discord Bot to **EXPOSE THEM!**
+
+With This New Discord Bot, You Can Make Sure They Get **SHAMMED** Into Joining VC With You!
+
+## Setting Up .env
+------
+
+Once you download this bot / the entire app, make sure you create a file named `.env` and inside there copy everything here
+
+``` .env
+DISC_USER_NAME=
+RBX_USER_ID=
+
+CUSTOM_MSG_BEFORE_NAME=
+CUSTOM_MSG_AFTER_NAME=
+
+DISC_BOT_TOKEN=
+
+SNITCH_CHANNEL_ID=
+DELAY_PER_MSG=
+
+LOOP_SPEED = 
+```
+
+Here you can add anything that you want to change without changing the code.
+
+**WHAT EACH ITEM MEANS**
+
+``` .env
+DISC_USER_NAME= The Persons Name In Discord
+RBX_USER_ID= Their Roblox User ID
+
+CUSTOM_MSG_BEFORE_NAME= A MSG That Displays BEFORE Their Username When Caught
+CUSTOM_MSG_AFTER_NAME= A  MSG That Displays AFTER Their Username When Caught
+
+DISC_BOT_TOKEN= The Discord Token For The BOT
+
+SNITCH_CHANNEL_ID= What Channel Should The Message Be Displayed In
+DELAY_PER_MSG= How Long Till The Next Message (MINS)
+
+LOOP_SPEED = How Long TIll The Next Update (MS)
+```
+
+## How It Works?
+------
+
+Every 'tick' the bot requests the user's presence from roblox via this link `https://presence.roblox.com/v1/presence/last-online`
+It also posts the user's id along with it. Then if the date is the same as todays date (UTC), we just send a message and wait x mins before we do it again.
---
+++ index.js
@@ -0,0 +1,49 @@
+const dotenv = require("dotenv").config();
+const discordjs = require("discord.js");
+const SnitchID = process.env.SNITCH_CHANNEL_ID;
+const BotToken = process.env.DISC_BOT_TOKEN;
+const DelayMin = process.env.DELAY_PER_MSG;
+const UserRBXID = process.env.RBX_USER_ID;
+const UserDiscID = process.env.DISC_USER_NAME;
+const LoopSpeed = process.env.LOOP_SPEED;
+const UsersName = process.env.USERS_NAME;
+
+var cDelay = 0;
+
+if(SnitchID == "") console.error("Channel ID Invalid");
+if(BotToken == "") console.error("Bot ID Invalid");
+
+const client = new discordjs.Client({ intents: [discordjs.GatewayIntentBits.Guilds,discordjs.GatewayIntentBits.GuildMembers] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    setInterval(mainLoop, LoopSpeed);
+});  
+
+client.login(BotToken);
+
+async function mainLoop(){
+    cDelay -= LoopSpeed;
+    (async () => {
+        const rawResponse = await fetch('https://presence.roblox.com/v1/presence/last-online', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({userIds: [UserRBXID]})
+        });
+        const content = await rawResponse.json();
+      
+        if(content.lastOnlineTimestamps.length > 0){
+            let lastOnline = new Date(content.lastOnlineTimestamps[0].lastOnline);
+            if(lastOnline.getUTCDay() == new Date().getUTCDay() && cDelay <= 0){
+                cDelay = DelayMin * 60000
+                msg = process.env.CUSTOM_MSG_BEFORE_NAME + UserDiscID + process.env.CUSTOM_MSG_AFTER_NAME;
+                client.channels.cache.get(SnitchID).send(msg);
+                console.log("Sent Message...");
+                console.log(`Waiting ${DelayMin} mins Before Next Message.`);
+            }
+        }
+      })();
+}
